237c6d101ade959f025398d6bd07d22e49a53e7e
Alper <alper.demir.fener@gmail.com>
2025-08-01 19:19:15 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,19 @@ import sequelize from "./api/v1/config/db.config.js";
 import User from "./api/v1/models/user.model.js";
 import authRoutes from "./api/v1/routes/auth.routes.js"
 import analysisRoutes from "./api/v1/routes/analysis.routes.js"
+import { clientIpMiddleware } from "./api/v1/middlewares/clientIp.middleware.js";
+import { requestLogger } from "./api/v1/logging/request.logger.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.set('trust proxy', true);
+
+app.use(clientIpMiddleware);
+app.use(requestLogger);
 
 app.use("/api/v1", authRoutes);
 app.use("/api/v1", analysisRoutes);
