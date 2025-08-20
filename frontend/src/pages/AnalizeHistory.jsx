2185b2b60311aa6c741014743a1255d3b75e2a98
Alper <alper.demir.fener@gmail.com>
2025-08-20 12:32:28 +0300

    Update AnalizeHistory.jsx

--- frontend/src/pages/AnalizeHistory.jsx
+++ frontend/src/pages/AnalizeHistory.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router";
+import { getAnalyzes } from "../services/analizeService";
 import {
     Table,
     TableBody,
@@ -7,32 +10,34 @@ import {
     TableHead,
     TableHeader,
     TableRow,
-} from "@/components/ui/table"
-import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination"
-
-import { useEffect, useState } from "react";
-import { Link, useSearchParams } from "react-router"
-import { getAnalyzes } from "../services/analizeService";
-
+} from "@/components/ui/table";
+import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const AnalizeHistory = () => {
-
     const limit = 10;
     const [page, setPage] = useState(1);
     const [totalRecord, setTotalRecord] = useState(null);
     const [analyzes, setAnalyzes] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const [searchParams, setSearchParams] = useSearchParams();
 
     const analizes = async () => {
-        const response = await getAnalyzes(page, limit);
-        const data = await response.json();
-        console.log(data);
-        if (response.ok) {
-            setTotalRecord(data.count);
-            setAnalyzes(data.rows);
+        setLoading(true);
+        try {
+            const response = await getAnalyzes(page, limit);
+            const data = await response.json();
+            if (response.ok) {
+                setTotalRecord(data.count);
+                setAnalyzes(data.rows);
+            }
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setLoading(false);
         }
-    }
+    };
 
     useEffect(() => {
         const currentPage = parseInt(searchParams.get("page") || "1", 10);
@@ -48,23 +53,21 @@ const AnalizeHistory = () => {
     return (
         <Table>
             <TableCaption>
-                {
-                    analyzes.length <= 0 ? (
-                        <>There is no analysis record.</>
-                    ) : (
-                        <>
-                            A list of your video analyses.
-                            {totalRecord && (
-                                <>
-                                    {" Shown "}
-                                    {((page - 1) * limit) + 1}-{Math.min(page * limit, totalRecord)} of {totalRecord}
-                                </>
-                            )}
-                        </>
-                    )
-                }
-
+                {analyzes.length <= 0 ? (
+                    <>There is no analysis record.</>
+                ) : (
+                    <>
+                        A list of your video analyses.
+                        {totalRecord && (
+                            <>
+                                {" Shown "}
+                                {((page - 1) * limit) + 1}-{Math.min(page * limit, totalRecord)} of {totalRecord}
+                            </>
+                        )}
+                    </>
+                )}
             </TableCaption>
+
             <TableHeader>
                 <TableRow>
                     <TableHead className="w-[100px]">Video</TableHead>
@@ -76,28 +79,36 @@ const AnalizeHistory = () => {
                     <TableHead className="text-right">Token Usage</TableHead>
                 </TableRow>
             </TableHeader>
+
             <TableBody>
-                {analyzes.length > 0 && analyzes.map((analize, index) => (
-                    <TableRow key={index}>
-                        <TableCell className="font-medium">
-                            <Link to={`https://www.youtube.com/watch?v=${analize.videoId}`} target="_blank">
-                                <img
-                                    src={`https://img.youtube.com/vi/${analize.videoId}/maxresdefault.jpg`}
-                                    alt="YouTube Thumbnail"
-                                    className={`rounded-xl shadow-md w-full object-cover transition-opacity duration-300
-                                }`}
-                                /></Link>
+                {loading ? (
+                    <TableRow>
+                        <TableCell colSpan={7} className="text-center py-10">
+                            <LoadingSpinner />
                         </TableCell>
-                        <TableCell>{analize.positive_comment_count}</TableCell>
-                        <TableCell>{analize.negative_comment_count}</TableCell>
-                        <TableCell>{analize.neutral_comment_count}</TableCell>
-                        <TableCell>{analize.total_comment_count}</TableCell>
-                        <TableCell>
-                            {new Date(analize.createdAt).toLocaleDateString("en-GB")}
-                        </TableCell>
-                        <TableCell className="text-right">{analize.total_token}</TableCell>
                     </TableRow>
-                ))}
+                ) : (
+                    analyzes.length > 0 &&
+                    analyzes.map((analize, index) => (
+                        <TableRow key={index}>
+                            <TableCell className="font-medium">
+                                <Link to={`https://www.youtube.com/watch?v=${analize.videoId}`} target="_blank">
+                                    <img
+                                        src={`https://img.youtube.com/vi/${analize.videoId}/maxresdefault.jpg`}
+                                        alt="YouTube Thumbnail"
+                                        className="rounded-xl shadow-md w-full object-cover transition-opacity duration-300"
+                                    />
+                                </Link>
+                            </TableCell>
+                            <TableCell>{analize.positive_comment_count}</TableCell>
+                            <TableCell>{analize.negative_comment_count}</TableCell>
+                            <TableCell>{analize.neutral_comment_count}</TableCell>
+                            <TableCell>{analize.total_comment_count}</TableCell>
+                            <TableCell>{new Date(analize.createdAt).toLocaleDateString("en-GB")}</TableCell>
+                            <TableCell className="text-right">{analize.total_token}</TableCell>
+                        </TableRow>
+                    ))
+                )}
             </TableBody>
 
             <TableFooter>
@@ -105,7 +116,6 @@ const AnalizeHistory = () => {
                     <TableCell colSpan={7}>
                         <Pagination>
                             <PaginationContent>
-                                {/* Previous */}
                                 <PaginationItem>
                                     <PaginationPrevious
                                         href={`?page=${page - 1}`}
@@ -113,14 +123,11 @@ const AnalizeHistory = () => {
                                         className={page <= 1 ? "pointer-events-none opacity-50" : ""}
                                         onClick={(e) => {
                                             e.preventDefault();
-                                            if (page > 1) {
-                                                setSearchParams({ page: page - 1 });
-                                            }
+                                            if (page > 1) setSearchParams({ page: page - 1 });
                                         }}
                                     />
                                 </PaginationItem>
 
-                                {/* Sayfa Numaraları */}
                                 {Array.from({ length: totalPages }).map((_, i) => (
                                     <PaginationItem key={i}>
                                         <PaginationLink
@@ -136,7 +143,6 @@ const AnalizeHistory = () => {
                                     </PaginationItem>
                                 ))}
 
-                                {/* Next */}
                                 <PaginationItem>
                                     <PaginationNext
                                         href={`?page=${page + 1}`}
@@ -144,9 +150,7 @@ const AnalizeHistory = () => {
                                         className={page >= totalPages ? "pointer-events-none opacity-50" : ""}
                                         onClick={(e) => {
                                             e.preventDefault();
-                                            if (page < totalPages) {
-                                                setSearchParams({ page: page + 1 });
-                                            }
+                                            if (page < totalPages) setSearchParams({ page: page + 1 });
                                         }}
                                     />
                                 </PaginationItem>
@@ -155,9 +159,8 @@ const AnalizeHistory = () => {
                     </TableCell>
                 </TableRow>
             </TableFooter>
-
         </Table>
-    )
-}
+    );
+};
 
-export default AnalizeHistory
+export default AnalizeHistory;
