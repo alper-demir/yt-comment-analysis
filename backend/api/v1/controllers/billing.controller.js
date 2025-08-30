6662d1a15b539095441b68a14bbdc2d02d82b0a9
Alper <alper.demir.fener@gmail.com>
2025-08-30 16:29:23 +0300

    Update billing.controller.js

--- backend/api/v1/controllers/billing.controller.js
+++ backend/api/v1/controllers/billing.controller.js
@@ -2,7 +2,7 @@ import TokenPlan from "../models/tokenPlan.model.js";
 
 export const getTokenPlans = async (req, res) => {
     try {
-        const plans = await TokenPlan.findAll();
+        const plans = await TokenPlan.findAll({ order: [["tokens", "ASC"]], });
         if (!plans) return res.status(404).json({ message: "Plans not found" });
         res.json(plans);
     } catch (error) {
